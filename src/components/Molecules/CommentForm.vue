cc6816ead79b191e6dbc90e6444606b35d7a0903
daisukey623 <42987402+daisukey623@users.noreply.github.com>
2021-06-20 12:41:44 +0900

    Add comment

---
+++ src/components/Molecules/CommentForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <form>
+<article class="media">
+
+  <div class="media-content">
+    <div class="field">
+      <p class="control">
+        <textarea class="textarea" placeholder="質問の回答を入力してください"></textarea>
+      </p>
+    </div>
+    <div class="field">
+      <p class="control">
+        <button class="button">回答を送信</button>
+      </p>
+    </div>
+  </div>
+</article>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+  }
+  }
+};
+</script>
+
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import firebase from 'firebase/app';
+import Vuex from "vuex";
 import "firebase/auth";
 import 'firebase/firestore';
 import store from '@/store/store.js'
@@ -27,6 +28,7 @@ firebase.initializeApp(firebaseConfig);
 new Vue({
   router,
   store,
+  Vuex,
   render: h => h(App)
 }).$mount('#app');
 
