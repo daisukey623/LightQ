e57f6b421ffc48db35ec8bf95035cc0e54962262
daisukey623 <42987402+daisukey623@users.noreply.github.com>
2021-07-04 16:41:37 +0900

    Add Logo
    ヘッダーにロゴを追加

--- src/components/Organisms/Header.vue
+++ src/components/Organisms/Header.vue
@@ -1,44 +1,60 @@
 <template>
-<div>
-  <nav class="navbar" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-    <a class="navbar-item" href="https://bulma.io">
-      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-    </a>
+  <div>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <div >
+          <a class="navbar-item pb-0"  @click="toHome">
+            <img src="@/assets/Logo.png" />
+          </a>
 
-    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
-  </div>
+          <small class="font-size-small has-text-grey-dark ml-3">
+            人事が人事を支援する投稿サイト</small
+          >
+        </div>
 
-  <div id="navbarBasicExample" class="navbar-menu">
-    <div class="navbar-start">
-     
+        <a
+          role="button"
+          class="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
       </div>
-    </div>
 
-    <div class="navbar-end">
-      <div class="navbar-item">
-        <button class="button is-light" @click="logOut()">ログアウト</button>
+      <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start"></div>
       </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <button class="button is-light" @click="logOut()">ログアウト</button>
+        </div>
+      </div>
+    </nav>
   </div>
-</nav>
-</div>
 </template>
 
 <script>
 import { auth } from '/src/main.js';
 
 export default {
-    methods: {
+  methods: {
     logOut: async function() {
       await auth.signOut().then(this.$store.dispatch('resetStore'));
     },
+    toHome() {
+      this.$router.push('/home');
+    },
   },
 };
-
 </script>
 
-<style scoped></style>
+<style scoped>
+.font-size-small {
+  font-size: 10px;
+}
+</style>
