521fba84a2251ef94dc0d0f50ebaf24bdae1f960
TakehiroTada <taketada.works.gmail.com>
2021-10-09 21:32:54 +0900

    feat: create example-component

---
+++ conflict/ExampleComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <span>あいうえお</span>
+    <span>かきくけこ</span>
+    <span>さしすせそ</span>
+    <span>たちつてと</span>
+    <span>なにぬねの</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      hoge: "hogehoge",
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
